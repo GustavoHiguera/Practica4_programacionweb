6cdab46ef9f20b1af22423d4531c48c6958e2d7c
GustavoHiguera <73560278+GustavoHiguera@users.noreply.github.com>
2022-02-27 17:29:34 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,40 +1,18 @@
-let obj = {};
-//Un objeto literal. Al ejecutarlo así no sucede nada
+//Si se escribe sin el require, al utilizar node app no sucede nada.
+let greet = require('./greet');
+//Una vez con el require, al utilizar node app me muestra el mensaje que tiene
+//greet.js: Hello from the module.
+greet();
+//Si ejecutamos el greet nos da un erro, puesto que el acceso al módulo es protegido
+//y en app.js no lo reconoce como una función.
+//Inclusive si declaramos greet como el require, nos dirá que no es una función, a menos
+//que en el módulo así este declarado mediante un module.exports.
 
-let persona = {
-    nombre: `Gustavo`,
-    apellido: `Higuera`,
-    getName: function () {
-        return `${this.nombre} ${this.apellido}`
-    },
-    domicilio: {
-        calle: `Madero #144`,
-        colonia: `Centro`,
-        cp: 28000,
-        municipio: `Colima`
-    },
-    getAddress: function () {
-        return `${this.domicilio.calle} Colonia: ${this.domicilio.colonia} CP: ${this.domicilio.cp}, ${this.domicilio.municipio}`
-    }
-}
-console.log(persona);
-//Al ejecutarlo así, me muestra todo el objeto, incluidas las llaves o los curly braces
 
-console.log(persona.nombre);
-console.log(persona.apellido);
-//Al ejecutarlo así solo me muestra el contenido de los atributos que selecciono,
-//es decir, solo me muestra el nombre y el apellido respectivamente.
+//Un objeto de javascript es una colección de nombres/valores pares,
+//que puede contener un objeto:
+//-Datos primitivos
+//Funciones(Métodos)
+//Otros objetos
 
-console.log(persona.getName());
-//Al ejecutarlo de esta forma, con esta función dentro del objeto, ya me muestra
-//el nombre y el apellido ordenado, y lo llamo como una función dentro del mismo objeto.
-
-console.log(persona.domicilio);
-//Al ejecutarlo de esta forma me muestra solo los datos del apartado domicilio,
-//y lo que contienen.
-
-console.log(persona.getAddress());
-//Al crear una función que me permita acceder al objeto domicilio de la persona, y que a su vez
-//me permita acceder a los atributos del objeto domicilio podemos ordenar con una template string
-//los datos y presentarlos de una manera ordenada, al igual que lo pide el ejercicio, mediante la
-//función getAddress() del objeto persona.
+//Un objeto literal es un nombre/valor rodeado de llaves.
